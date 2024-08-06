1e33d1f196f214bb4df3c6d0f9d5c2e4612db923
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-06 20:27:06 +0300

    find the maximum Element in an array

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
+import java.util.*
+
+//find the maximum Element
+
 fun main() {
-    println("Hello World!")
+    val reader = Scanner(System.`in`)
+    println("Enter size of the array or max number of values")
+    val n = reader.nextInt()
+    val numbers   =  IntArray(n)
+
+    for (i in 0 until n){
+        println("Enter the precise value for element $i")
+        numbers[i] = reader.nextInt()
+    }
+    val maxValue = numbers.maxOrNull()
+    if (maxValue != null){
+        println("The maximum value of the array is $maxValue")
+    }else{
+        println("The array is empty:")
+    }
 }
