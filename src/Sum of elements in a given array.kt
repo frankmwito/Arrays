bae7554d506bb37deef7fd6a2fc3c089a957c02d
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-06 20:36:56 +0300

    Sum of elements in a given array

---
+++ src/Sum of elements in a given array.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+//sum of elements in a given array
+
+fun main()  {
+    println("Enter the size of the array")
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+    val numbers = IntArray(n)
+
+    for (i in 0..<n){
+        println("Enter the value of position $i")
+        numbers[i] = reader.nextInt()
+    }
+    val sum = numbers.sum()
+
+    if (sum != null){
+        println("The sum is:  $sum")
+    }
+    else{
+        println("The string is empty")
+    }
+}
