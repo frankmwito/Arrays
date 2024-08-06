059103ca1e81ca29373a8df63c9fd9f3e6d1fa7d
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-06 21:31:43 +0300

    find the second-largest element in an Array

--- src/Find the second largest Element.kt
+++ src/Find the second largest Element.kt
@@ -1,1 +1,27 @@
-// find the second-largest element in an Array
+import java.util.*
+
+// Find the second-largest element in an array
+
+fun main() {
+    println("Enter the size of the array")
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+
+    // Check if the array has at least two elements
+    if (n < 2) {
+        println("Array should have at least two elements")
+        return
+    }
+
+    val numbers = IntArray(n)
+
+    for (i in 0 until n) {
+        println("Enter the value of position $i")
+        numbers[i] = reader.nextInt()
+    }
+
+    // Sort the array and reverse it to get descending order
+    val sortedDescending = numbers.sortedDescending()
+    println("The second largest element is ${sortedDescending[1]}")
+}
+
