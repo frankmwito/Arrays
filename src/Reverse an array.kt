b593ba57a43aa5a6c19be9445e54c4acd3fe6d58
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-06 20:47:02 +0300

    Reverse an array

--- src/Reverse an array.kt
+++ src/Reverse an array.kt
@@ -1,2 +1,17 @@
+import java.util.Scanner
+
 // reverse an array
 
+fun main()  {
+    println("Enter the size of the array")
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+    val numbers = IntArray(n)
+
+    for (i in 0 until n){
+        println("Enter the value in position $i")
+        numbers[i] = reader.nextInt()
+    }
+    val reverse = numbers.joinToString().reversed()
+    println("The reversed array is $reverse")
+}
