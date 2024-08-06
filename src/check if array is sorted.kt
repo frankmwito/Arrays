48765e065e36f705f86bf2ec929cef1177451124
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-06 21:11:53 +0300

    check if array is sorted

--- src/check if array is sorted.kt
+++ src/check if array is sorted.kt
@@ -1,1 +1,27 @@
+import java.util.*
+
 //check if array is sorted
+
+fun main() {
+    println("Enter the size of the array")
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+    val numbers = IntArray(n)
+
+    for (i in 0..<n){
+        println("Enter the value of position $i")
+        numbers[i] = reader.nextInt()
+    }
+    val isSorted = isArraySorted(numbers)
+    println("$isSorted")
+
+    }
+
+fun isArraySorted(arr: IntArray):Boolean {
+    for (i in 0 until arr.size-1){
+        if (arr[i] > arr[i+1]){
+            return false
+        }
+    }
+    return true
+}
